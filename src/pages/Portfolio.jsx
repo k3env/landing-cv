c7e4689f38936cd5229a701a91822d31a0f0394a
Sergey Tulonov <me@k3env.site>
2023-03-29 23:11:42 +0800

    feat: add binding to projects endpoint

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Slider from 'react-slick';
-import { Col, Row, Modal, Badge, Container, Image, ListGroup, Stack, Button } from 'react-bootstrap';
-import { DocumentRenderer } from '@keystone-6/document-renderer';
+import { Col, Row, Modal, Badge, Container, Image, Button } from 'react-bootstrap';
+import ReactHtmlParser from 'react-html-parser';
 import { SectionHeading } from '../components/SectionHeading';
 
 function TagSection({ filter, setFilter }) {
@@ -22,12 +22,19 @@ function TagSection({ filter, setFilter }) {
         >
           <FilterButton filter="*" label="Всё" active={filter === '*'} />
           {tags.map((f) => (
-            // eslint-disable-next-line no-underscore-dangle
             <FilterButton key={f._id} filter={f._id} label={f.label} active={filter === f._id} />
           ))}
         </ul>
       </Col>
     </Row>
+  );
+}
+
+function FilterButton({ active, label, filter }) {
+  return (
+    <Button as="li" size="sm" variant="outline-primary" active={active} className="m-1" data-filter={filter}>
+      {label}
+    </Button>
   );
 }
 
@@ -46,7 +53,7 @@ function PortfolioItem({ title, cover, images, description, tags }) {
   return (
     <Col lg={4} md={6} className="mb-4 portfolio-item">
       <div className="position-relative overflow-hidden mb-2">
-        <img className="img-fluid rounded w-100" src={cover.url} alt="" />
+        <Image fluid rounded width="100%" src={cover.url} alt="" />
         <div className="portfolio-btn bg-primary d-flex align-items-center justify-content-center">
           <a onClick={() => setShow(true)}>
             <i className="fa fa-plus text-white" style={{ fontSize: '60px' }} />
@@ -61,21 +68,24 @@ function PortfolioItem({ title, cover, images, description, tags }) {
               <Row>
                 <Col xs={12} md={6} className="mb-5">
                   <Slider {...settings}>
+                    <div key={cover._id} className="position-relative mb-2">
+                      <Image fluid rounded width="100%" src={cover.url} alt="" />
+                    </div>
                     {images.map((i) => (
-                      <div key={i.id} className="position-relative mb-2">
-                        <Image fluid rounded width="100%" src={i.image.url} alt="" style={{ width: '100px' }} />
+                      <div key={i._id} className="position-relative mb-2">
+                        <Image fluid rounded width="100%" src={i.url} alt="" />
                       </div>
                     ))}
                   </Slider>
                 </Col>
                 <Col xs={12} md={6}>
-                  <DocumentRenderer document={description.document} />
+                  <div>{ReactHtmlParser(description)}</div>
                 </Col>
                 <Row>
                   <Col xs={12}>
                     {tags.map((t) => (
-                      <Badge pill text="light" key={t.id} className="p-2 mx-1">
-                        {t.name}
+                      <Badge pill text="light" key={t._id} className="p-2 mx-1">
+                        {t.label}
                       </Badge>
                     ))}
                   </Col>
@@ -89,24 +99,12 @@ function PortfolioItem({ title, cover, images, description, tags }) {
   );
 }
 
-function FilterButton({ active, label, filter }) {
-  const classList = ['btn', 'btn-sm', 'btn-outline-primary', 'm-1'];
-  if (active) {
-    classList.push('active');
-  }
-  return (
-    <li className={classList.join(' ')} data-filter={filter}>
-      {label}
-    </li>
-  );
-}
-
 export function Portfolio({ hidden }) {
   if (hidden) return <div />;
   const [filter, setFilter] = useState('*');
   const [projects, setProjects] = useState([]);
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/projects`)
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/projects?aggregated`)
       .then((r) => r.json())
       .then((v) => setProjects(v.data));
   }, []);
@@ -116,9 +114,9 @@ export function Portfolio({ hidden }) {
       <Container>
         <SectionHeading bgText="Галерея" title="Портфолио" />
         <TagSection filter={filter} setFilter={setFilter} />
-        <Row className="row portfolio-container">
+        <Row className="portfolio-container">
           {projects.map((p) => (
-            <PortfolioItem key={p.id} {...p} />
+            <PortfolioItem key={p._id} {...p} />
           ))}
         </Row>
       </Container>
